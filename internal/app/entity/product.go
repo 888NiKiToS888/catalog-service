c6fe2476c992f0dac9e374e66e2e1d701be748f7
Nikita <nikita.yurchenko52@mail.ru>
2026-06-17 17:59:52 +0300

    TASK-1005: Добавил сервисный слой и HTTP-хендлеры для категорий и продуктов

--- internal/app/entity/product.go
+++ internal/app/entity/product.go
@@ -19,3 +19,57 @@ type Product struct {
 	CreatedAt    time.Time `bun:"created_at,notnull"`
 	UpdatedAt    time.Time `bun:"updated_at,notnull"`
 }
+
+////////////////////////////////////////////////////////////////////////////////
+///// HTTP REQUEST & RESPONSE //////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+type RequestProductCreate struct {
+	Name         string    `json:"name"`
+	Description  *string   `json:"description"`
+	Price        float64   `json:"price"`
+	CategoryGUID uuid.UUID `json:"category_guid"`
+}
+
+func (r RequestProductCreate) Validate() error {
+	if r.Name == "" {
+		return ErrIncorrectParameters
+	}
+	if r.Price <= 0 {
+		return ErrIncorrectParameters
+	}
+	if r.CategoryGUID.IsNil() {
+		return ErrIncorrectParameters
+	}
+	return nil
+}
+
+type RequestProductUpdate struct {
+	Name         *string    `json:"name"`
+	Description  *string    `json:"description"`
+	Price        *float64   `json:"price"`
+	CategoryGUID *uuid.UUID `json:"category_guid"`
+}
+
+func (r RequestProductUpdate) Validate() error {
+	if r.Price != nil && *r.Price <= 0 {
+		return ErrIncorrectParameters
+	}
+	if r.CategoryGUID != nil && r.CategoryGUID.IsNil() {
+		return ErrIncorrectParameters
+	}
+	if r.Name != nil && *r.Name == "" {
+		return ErrIncorrectParameters
+	}
+	return nil
+}
+
+type ResponseProduct struct {
+	GUID         uuid.UUID `json:"guid"`
+	Name         string    `json:"name"`
+	Description  *string   `json:"description"`
+	Price        float64   `json:"price"`
+	CategoryGUID uuid.UUID `json:"category_guid"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
